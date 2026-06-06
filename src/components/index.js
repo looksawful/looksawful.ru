4a2c6ea23dc1e80cb52ec46ae8624b8fdd558d6f
looksawful <118225585+looksawful@users.noreply.github.com>
2026-06-06 13:37:26 +0300

    refactor: remove navigation initializers

--- src/components/index.js
+++ src/components/index.js
@@ -2,14 +2,11 @@ import { mountawfulface } from "./awfulface/awfulface.js";
 import { initCvGroupAnimations } from "./cv-group-animations/cv-group-animations.js";
 import { initCvInlineVideos } from "./cv-inline-video/cv-inline-video.js";
 import { mountCvProjectLogos } from "./cv-project-logos/cv-project-logos.js";
-import { initCvSidebar } from "./cv-sidebar/cv-sidebar.js";
 import { mountCvDemoVisuals } from "./cv-task-previews/cv-task-visual-demos.js";
 import { initHeroTitleAnimation } from "./hero-title/hero-title.js";
-import { initPageNavigation } from "./page-navigation/page-navigation.js";
 import { initPreloadStates } from "./preload-state/preload-state.js";
 import { initSystemMotion } from "./system-motion/system-motion.js";
 import { renderCvExperience } from "../sections/cv/cv-renderer.js";
-import { initSiteNavigationState } from "../sections/site-navigation/site-navigation.js";
 
 function runComponentStep(label, callback) {
   try {
@@ -20,21 +17,14 @@ function runComponentStep(label, callback) {
   }
 }
 
-function mountFaces() {
-  mountawfulface("awfulface-hero", { fallOnScroll: true });
-}
-
 export function initComponents() {
-  runComponentStep("initPreloadStates", () => initPreloadStates());
-  runComponentStep("initSiteNavigationState", () => initSiteNavigationState());
-  runComponentStep("initHeroTitleAnimation", () => initHeroTitleAnimation());
-  runComponentStep("mountawfulface", () => mountFaces());
   runComponentStep("renderCvExperience", () => renderCvExperience());
+  runComponentStep("mountawfulface", () => mountawfulface("awfulface-hero"));
+  runComponentStep("initHeroTitleAnimation", () => initHeroTitleAnimation());
   runComponentStep("mountCvProjectLogos", () => mountCvProjectLogos());
+  runComponentStep("mountCvDemoVisuals", () => mountCvDemoVisuals(document));
   runComponentStep("initCvGroupAnimations", () => initCvGroupAnimations());
   runComponentStep("initCvInlineVideos", () => initCvInlineVideos());
-  runComponentStep("mountCvDemoVisuals", () => mountCvDemoVisuals(document));
-  runComponentStep("initCvSidebar", () => initCvSidebar());
-  runComponentStep("initPageNavigation", () => initPageNavigation());
+  runComponentStep("initPreloadStates", () => initPreloadStates());
   runComponentStep("initSystemMotion", () => initSystemMotion());
 }
