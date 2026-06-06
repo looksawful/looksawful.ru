579d1a33e9868d6bfe028b18728232b149c80ea3
looksawful <118225585+looksawful@users.noreply.github.com>
2026-06-06 13:56:57 +0300

    perf: lazy load secondary components

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,6 @@
 import { mountawfulface } from "./awfulface/awfulface.js";
-import { initCvGroupAnimations } from "./cv-group-animations/cv-group-animations.js";
-import { initCvInlineVideos } from "./cv-inline-video/cv-inline-video.js";
 import { mountCvProjectLogos } from "./cv-project-logos/cv-project-logos.js";
-import { mountCvDemoVisuals } from "./cv-task-previews/cv-task-visual-demos.js";
 import { initHeroTitleAnimation } from "./hero-title/hero-title.js";
-import { initPreloadStates } from "./preload-state/preload-state.js";
 import { initSystemMotion } from "./system-motion/system-motion.js";
 import { renderCvExperience } from "../sections/cv/cv-renderer.js";
 
@@ -17,14 +13,38 @@ function runComponentStep(label, callback) {
   }
 }
 
+function runIdle(callback) {
+  if ("requestIdleCallback" in window) {
+    window.requestIdleCallback(callback, { timeout: 1400 });
+    return;
+  }
+
+  window.setTimeout(callback, 0);
+}
+
+async function initLazyComponents() {
+  const [{ mountCvDemoVisuals }, { initCvGroupAnimations }, { initCvInlineVideos }, { initPreloadStates }] =
+    await Promise.all([
+      import("./cv-task-previews/cv-task-visual-demos.js"),
+      import("./cv-group-animations/cv-group-animations.js"),
+      import("./cv-inline-video/cv-inline-video.js"),
+      import("./preload-state/preload-state.js"),
+    ]);
+
+  runComponentStep("mountCvDemoVisuals", () => mountCvDemoVisuals(document));
+  runComponentStep("initCvGroupAnimations", () => initCvGroupAnimations());
+  runComponentStep("initCvInlineVideos", () => initCvInlineVideos());
+  runComponentStep("initPreloadStates", () => initPreloadStates());
+}
+
 export function initComponents() {
   runComponentStep("renderCvExperience", () => renderCvExperience());
   runComponentStep("mountawfulface", () => mountawfulface("awfulface-hero"));
   runComponentStep("initHeroTitleAnimation", () => initHeroTitleAnimation());
   runComponentStep("mountCvProjectLogos", () => mountCvProjectLogos());
-  runComponentStep("mountCvDemoVisuals", () => mountCvDemoVisuals(document));
-  runComponentStep("initCvGroupAnimations", () => initCvGroupAnimations());
-  runComponentStep("initCvInlineVideos", () => initCvInlineVideos());
-  runComponentStep("initPreloadStates", () => initPreloadStates());
   runComponentStep("initSystemMotion", () => initSystemMotion());
+
+  runIdle(() => {
+    void initLazyComponents();
+  });
 }
