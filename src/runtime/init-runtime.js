b5625b752cc57c7cb4d0f18d5bb51536466b2ac6
Ivan Krushinski <118225585+looksawful@users.noreply.github.com>
2026-07-12 17:48:28 +0300

    Mount Jestei type inside brand bento

--- src/runtime/init-runtime.js
+++ src/runtime/init-runtime.js
@@ -2,6 +2,7 @@ import { mountAll } from "./mount-engine.js";
 import { MOUNTS } from "./mounts.js";
 import { removeObsoleteCopy } from "./remove-obsolete-copy.js";
 import { reorganizeJesteiLogoMedia } from "./reorganize-jestei-logo-media.js";
+import { mergeJesteiTypeIntoLogo } from "./merge-jestei-type-into-logo.js";
 
 let initialized = false;
 
@@ -13,5 +14,6 @@ export async function initRuntime(root = document) {
   initialized = true;
   removeObsoleteCopy(root);
   reorganizeJesteiLogoMedia(root);
+  mergeJesteiTypeIntoLogo(root);
   await mountAll(root, MOUNTS);
 }
