d207292d1508c2cba037b0b1e22dd9fe1d480e0f
Ivan Krushinski <118225585+looksawful@users.noreply.github.com>
2026-09-05 01:51:23 +0300

    feat: add privacy static page type

--- src/site/pages/types.ts
+++ src/site/pages/types.ts
@@ -12,7 +12,8 @@ export type EntityPageId =
   | `project:${ProjectId}`
   | `collection:${CollectionId}`;
 
-export type SitePageId = "home" | "cv" | "not-found" | EntityPageId;
+export type StaticPageId = "cv" | "privacy";
+export type SitePageId = "home" | StaticPageId | "not-found" | EntityPageId;
 
 export interface VitePageBuild {
   kind: "vite";
@@ -24,7 +25,7 @@ export interface PublicStaticPageBuild {
 }
 
 export type SitePageBuild = VitePageBuild | PublicStaticPageBuild;
-export type SitePageRenderer = "home" | "entity" | "cv" | "not-found";
+export type SitePageRenderer = "home" | "entity" | "cv" | "privacy" | "not-found";
 
 interface BasePageDefinition {
   id: SitePageId;
@@ -66,12 +67,21 @@ export interface CollectionPageDefinition extends BasePageDefinition {
   build: VitePageBuild;
 }
 
-export interface StaticPageDefinition extends BasePageDefinition {
+export interface CvPageDefinition extends BasePageDefinition {
   type: "static";
   id: "cv";
   renderer: "cv";
   build: PublicStaticPageBuild;
 }
+
+export interface PrivacyPageDefinition extends BasePageDefinition {
+  type: "static";
+  id: "privacy";
+  renderer: "privacy";
+  build: PublicStaticPageBuild;
+}
+
+export type StaticPageDefinition = CvPageDefinition | PrivacyPageDefinition;
 
 export interface NotFoundPageDefinition extends BasePageDefinition {
   type: "not-found";
