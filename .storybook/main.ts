1b72789293b4b188ebe708890c040553ea3cc0be
Ivan Krushinsky <118225585+looksawful@users.noreply.github.com>
2026-09-10 21:59:39 +0300

    feat(lab): add Storybook design system viewer

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+const config = {
+  stories: ["../src/lab/stories/**/*.stories.@(js|mjs)"],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "storybook-design-token",
+  ],
+  framework: {
+    name: "@storybook/html-vite",
+    options: {},
+  },
+  staticDirs: [{ from: "../public", to: "/" }],
+  docs: {
+    autodocs: "tag",
+  },
+};
+
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,27 @@
+import "../src/styles/index.css";
+
+const preview = {
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+    options: {
+      storySort: {
+        order: [
+          "00 Foundations",
+          "01 Atoms",
+          "02 Molecules",
+          "03 Organisms",
+          "04 Templates",
+          "05 Pages",
+          "06 Motion",
+          "90 Experimental",
+        ],
+      },
+    },
+    a11y: {
+      test: "todo",
+    },
+  },
+};
+
+export default preview;
