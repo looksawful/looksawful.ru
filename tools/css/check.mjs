09bae3fab7151a74b94ccbb126d73037c6051b79
Ivan Krushinsky <118225585+looksawful@users.noreply.github.com>
2026-09-13 16:53:25 +0300

    test(lab): accept gallery preview contracts

--- tools/css/check.mjs
+++ tools/css/check.mjs
@@ -180,6 +180,7 @@ const EXPECTED_IMPORT_GRAPH = Object.freeze([
   '@import "./slider.css" layer(components);',
   '@import "./media-deck.css" layer(components);',
   '@import "./media-lightbox.css" layer(components);',
+  '@import "./gallery.css" layer(components);',
   '@import "../components/jestei-theme-organism/jestei-theme-organism.css";',
   '@import "./captions.css" layer(captions);',
   '@import "./motion.css" layer(motion);',
