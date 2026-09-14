b3e4f01f49f724ab7eb2fed1326314cf2704a117
Ivan Krushinsky <118225585+looksawful@users.noreply.github.com>
2026-09-14 16:22:20 +0300

    test: point Pet Projects contract at canonical data module

--- test/pet-project-cards.test.mjs
+++ test/pet-project-cards.test.mjs
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
 import test from "node:test";
 
-import { petProjectCards } from "../src/data/subproject-cards.ts";
+import { petProjectCards } from "../src/data/pet-project-cards.ts";
 import { renderPetProjectCards } from "../src/templates/subproject-card.ts";
 
 const approvedCards = [
