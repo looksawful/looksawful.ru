6a87ed3a6a86683e5825c033d94a33964ff94f7b
Ivan Krushinsky <118225585+looksawful@users.noreply.github.com>
2026-09-18 21:44:01 +0300

    feat: register KEYS page content

--- src/content/pages/index.ts
+++ src/content/pages/index.ts
@@ -7,6 +7,7 @@ import { awful3dMockupsPageContent } from "./projects/awful-3d-mockups.ts";
 import { awfulCasesPageContent } from "./projects/awful-cases.ts";
 import { awfulStudioPageContent } from "./projects/awful-studio.ts";
 import { berrySocialContentPageContent } from "./projects/berry-social-content-2020.ts";
+import { keysPageContent } from "./projects/keys.ts";
 import { movesAwfulPageContent } from "./projects/moves-awful.ts";
 import { createEntityPageContentRegistry } from "./registry.ts";
 import { validateEntityPageContents } from "./validation.ts";
@@ -25,6 +26,7 @@ export const entityPageContents = [
   awful3dMockupsPageContent,
   awfulStudioPageContent,
   berrySocialContentPageContent,
+  keysPageContent,
   movesAwfulPageContent,
 ] as const satisfies readonly EntityPageContent[];
 
