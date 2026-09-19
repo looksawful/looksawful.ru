3c00f84043d4c58e6501516d4f82f8a2011513a7
Ivan Krushinsky <118225585+looksawful@users.noreply.github.com>
2026-09-19 11:29:41 +0300

    feat: include studio and 3D review entries

--- src/data/media/entries/index.ts
+++ src/data/media/entries/index.ts
@@ -7,7 +7,9 @@ import {
   mediaUsageMetadataByEntryId,
 } from "../usage-records.ts";
 
+import { awful3dMockupsStaticMediaEntries } from "./awful-3d-mockups-static.ts";
 import { awfulCasesMediaEntries } from "./awful-cases.ts";
+import { awfulStudioStaticMediaEntries } from "./awful-studio-static.ts";
 import { awfulMockupsMediaEntries } from "./awful-mockups.ts";
 import { behanceShootingMediaEntries } from "./behance-shootings.ts";
 import { berryMediaEntries } from "./berry.ts";
@@ -31,8 +33,10 @@ import { unassignedMediaEntries } from "./unassigned.ts";
 import { usefulMediaEntries } from "./useful.ts";
 
 const rawMediaEntries = [
+  ...awful3dMockupsStaticMediaEntries,
   ...awfulCasesMediaEntries,
   ...awfulMockupsMediaEntries,
+  ...awfulStudioStaticMediaEntries,
   ...behanceShootingMediaEntries,
   ...berryMediaEntries,
   ...berserkTimerMediaEntries,
